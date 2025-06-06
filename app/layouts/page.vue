22824047d58f45d0280488ebd91208a292695fea
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-06-06 19:53:48 +0800

    Update page.vue

--- app/layouts/page.vue
+++ app/layouts/page.vue
@@ -1,5 +1,6 @@
 <template>
-    <main class="px-4 py-10 m-auto sm:px-8 sm:rounded-lg max-w-2xl sm:shadow">
+<div class="px-4 py-10 m-auto sm:px-8 sm:rounded-lg max-w-2xl sm:shadow">
+    <main class="px-4 mx-auto max-w-2xl prose">
       <slot>
         <h1 class="text-2xl">Page not found</h1>
       </slot>
