70c1323bd5cd863403a41252373fef864da1d8dd
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-06-06 19:50:38 +0800

    Add files via upload

---
+++ app/layouts/page.vue
@@ -0,0 +1,8 @@
+<template>
+    <main class="px-4 py-10 m-auto sm:px-8 sm:rounded-lg max-w-2xl sm:shadow">
+      <slot>
+        <h1 class="text-2xl">Page not found</h1>
+      </slot>
+    </main>
+  </div>
+</template>
